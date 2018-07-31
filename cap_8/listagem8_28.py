70306a53ecc63c701c4894992a95b601ce4fb355
Bruno Levy <brunolevy06@gmail.com>
2018-07-30 21:20:42 -0300

    mais listagens cap 8

---
+++ cap_8/listagem8_28.py
@@ -0,0 +1,6 @@
+def soma(a,b):
+    print(a+b)
+L=[2,3]
+
+# O asterisco serve para para indicar que queremos desempacotar a Lista L e utilizar seus valores como parametro para a funcao soma.
+soma(*L)
